3b80a71fda4ad88fd6ac642ebaa02ec583c03bf1
kccheatno1 <kc.cheatno1@gmail.com>
2026-05-21 20:25:52 +0800

    Auto dump: 2026-05-21 20:24:05

--- schemasystem_dll.rs
+++ schemasystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-21 10:31:07.766502300 UTC
+// 2026-05-14 02:03:12.374178800 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
