5e369710bbf69d745947454bfee6033f906d58d8
kccheatno1 <kc.cheatno1@gmail.com>
2026-05-21 18:37:48 +0800

    Auto dump: 2026-05-21 18:37:36

---
+++ buttons.hpp
@@ -0,0 +1,13 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-05-21 10:31:07.766502300 UTC
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    // Module: client.dll
+    namespace buttons {
+    }
+}
