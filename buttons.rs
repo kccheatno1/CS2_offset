d6332d51db458dd493a50042997e50a462384c96
kccheatno1 <kc.cheatno1@gmail.com>
2026-05-21 18:37:51 +0800

    Auto dump: 2026-05-21 18:37:36

---
+++ buttons.rs
@@ -0,0 +1,10 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-05-21 10:31:07.766502300 UTC
+
+#![allow(non_upper_case_globals, unused)]
+
+pub mod cs2_dumper {
+    // Module: client.dll
+    pub mod buttons {
+    }
+}
