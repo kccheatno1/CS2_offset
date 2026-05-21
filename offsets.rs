c796bab0ee2ee23c0496853905684d9604465671
kccheatno1 <kc.cheatno1@gmail.com>
2026-05-21 18:38:18 +0800

    Auto dump: 2026-05-21 18:37:36

---
+++ offsets.rs
@@ -0,0 +1,9 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-05-21 10:31:07.766502300 UTC
+
+#![allow(non_upper_case_globals, unused)]
+
+pub mod cs2_dumper {
+    pub mod offsets {
+    }
+}
